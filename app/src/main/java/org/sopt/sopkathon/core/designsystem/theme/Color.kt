0159c0ac056a9ddeebe7f6f93fe12cbebe5f39a2
Hyejeong <myejeong0211@gmail.com>
2025-05-17 22:25:15 +0900

    [setting/#3] 컬러 설정

--- app/src/main/java/org/sopt/sopkathon/core/designsystem/theme/Color.kt
+++ app/src/main/java/org/sopt/sopkathon/core/designsystem/theme/Color.kt
@@ -6,96 +6,113 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 
-// TODO: Temporary ColorScheme
-val primary = Color(0xFFFF153C)
-val black = Color(0xFF000000)
-val gray600 = Color(0xFF191919)
-val gray500 = Color(0xFF262626)
-val gray400 = Color(0xFF404040)
-val gray300 = Color(0xFF737373)
-val gray200 = Color(0xFF808080)
-val gray100 = Color(0xFFB3B3B3)
+val black = Color(0xFF121212)
 val white = Color(0xFFFFFFFF)
+
+// Gray scale
+val gray100 = Color(0xFFF5F5F5)
+val gray200 = Color(0xFFD9D9D9)
+val gray300 = Color(0xFFCCCCCC)
+val gray400 = Color(0xFFBABABA)
+val gray500 = Color(0xFFA6A6A6)
+val gray600 = Color(0xFF858585)
+val gray700 = Color(0xFF646464)
+val gray800 = Color(0xFF5C5C5C)
+val gray900 = Color(0xFF212121)
 
 @Stable
 class SopkathonColors(
-    primary: Color,
     black: Color,
-    gray600: Color,
-    gray500: Color,
-    gray400: Color,
-    gray300: Color,
-    gray200: Color,
-    gray100: Color,
     white: Color,
+    gray100: Color,
+    gray200: Color,
+    gray300: Color,
+    gray400: Color,
+    gray500: Color,
+    gray600: Color,
+    gray700: Color,
+    gray800: Color,
+    gray900: Color,
     isLight: Boolean,
 ) {
-    var primary by mutableStateOf(primary)
-        private set
     var black by mutableStateOf(black)
         private set
-    var gray600 by mutableStateOf(gray600)
-        private set
-    var gray500 by mutableStateOf(gray500)
-        private set
-    var gray400 by mutableStateOf(gray400)
-        private set
-    var gray300 by mutableStateOf(gray300)
-        private set
-    var gray200 by mutableStateOf(gray200)
+    var white by mutableStateOf(white)
         private set
     var gray100 by mutableStateOf(gray100)
         private set
-    var white by mutableStateOf(white)
+    var gray200 by mutableStateOf(gray200)
+        private set
+    var gray300 by mutableStateOf(gray300)
+        private set
+    var gray400 by mutableStateOf(gray400)
+        private set
+    var gray500 by mutableStateOf(gray500)
+        private set
+    var gray600 by mutableStateOf(gray600)
+        private set
+    var gray700 by mutableStateOf(gray700)
+        private set
+    var gray800 by mutableStateOf(gray800)
+        private set
+    var gray900 by mutableStateOf(gray900)
         private set
     var isLight by mutableStateOf(isLight)
 
     fun copy(): SopkathonColors = SopkathonColors(
-        primary,
         black,
-        gray600,
-        gray500,
-        gray400,
-        gray300,
-        gray200,
-        gray100,
         white,
+        gray100,
+        gray200,
+        gray300,
+        gray400,
+        gray500,
+        gray600,
+        gray700,
+        gray800,
+        gray900,
         isLight,
     )
 
     fun update(other: SopkathonColors) {
-        primary = other.primary
         black = other.black
-        gray600 = other.gray600
-        gray500 = other.gray500
-        gray400 = other.gray400
-        gray300 = other.gray300
-        gray200 = other.gray200
-        gray100 = other.gray100
         white = other.white
+        gray100 = other.gray100
+        gray200 = other.gray200
+        gray300 = other.gray300
+        gray400 = other.gray400
+        gray500 = other.gray500
+        gray600 = other.gray600
+        gray700 = other.gray700
+        gray800 = other.gray800
+        gray900 = other.gray900
         isLight = other.isLight
     }
 }
 
 fun SopkathonLightColors(
-    Primary: Color = primary,
     Black: Color = black,
-    Gray600: Color = gray600,
-    Gray500: Color = gray500,
-    Gray400: Color = gray400,
-    Gray300: Color = gray300,
-    Gray200: Color = gray200,
-    Gray100: Color = gray100,
     White: Color = white,
+    Gray100: Color = gray100,
+    Gray200: Color = gray200,
+    Gray300: Color = gray300,
+    Gray400: Color = gray400,
+    Gray500: Color = gray500,
+    Gray600: Color = gray600,
+    Gray700: Color = gray700,
+    Gray800: Color = gray800,
+    Gray900: Color = gray900,
 ) = SopkathonColors(
-    Primary,
     Black,
-    Gray600,
-    Gray500,
-    Gray400,
-    Gray300,
-    Gray200,
-    Gray100,
     White,
+    Gray100,
+    Gray200,
+    Gray300,
+    Gray400,
+    Gray500,
+    Gray600,
+    Gray700,
+    Gray800,
+    Gray900,
     isLight = true,
 )
